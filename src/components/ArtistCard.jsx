0c093456528f5b8911b5750ca9d7f082c5fe502d
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-22 15:48:31 -0700

    Requests from Spotify

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Card from "./Card";
+import ProfileSVG from "../assets/profile.svg";
 
-const ArtistCard = () => {
+const ArtistCard = ({ picSrc, name }) => {
   return (
     <Card w="7rem" h="11rem" br="0.5rem">
-      <p>Artist</p>
+      {picSrc ? <img src={picSrc} alt="Artist Pic" /> : <img src={ProfileSVG} alt="Artist Pic" />}
+      <p>{name ? name : "Artist"}</p>
     </Card>
-  )
-}
+  );
+};
 
 export default ArtistCard;
